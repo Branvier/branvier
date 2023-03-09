68745bc76e2676a86a3b3f8f93bea29b1035ba3b
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-03-09 20:34:55 -0300

    Added json extensions

--- lib/extensions/json.dart
+++ lib/extensions/json.dart
@@ -2,12 +2,42 @@ part of '/branvier.dart';
 
 extension JsonObject on Json {
   ///Converts to an indented [String].
-  String encode() => const JsonEncoder.withIndent('  ').convert(this);
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
 
   ///Logs a formatted json in the console.
-  void log() => dev.log(encode());
+  void log() => dev.log(toJson());
 }
 
 extension JsonString on String {
-  T decode<T>() => const JsonDecoder().convert(this);
+  ///Parses [this] String as [T].
+  T parse<T>() => const JsonDecoder().convert(this);
+
+  ///Tries to parse [this] String as [T].
+  T? tryParse<T>() {
+    try {
+      return parse<T>();
+    } catch (e) {
+      return null;
+    }
+  }
+}
+
+extension JsonInt on num {
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
+}
+
+extension JsonBool on bool {
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
+}
+
+extension JsonStringList on List<String> {
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
+}
+
+extension JsonNumList on List<num> {
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
+}
+
+extension JsonBoolList on List<bool> {
+  String toJson() => const JsonEncoder.withIndent('  ').convert(this);
 }

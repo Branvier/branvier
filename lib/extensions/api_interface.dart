76782a89417921d12a2a1107f5a7ec3dc80256b2
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-03-14 17:54:33 -0300

    added ApiResponse model/interface

--- lib/extensions/api_interface.dart
+++ lib/extensions/api_interface.dart
@@ -5,18 +5,33 @@ part of '/branvier.dart';
 ///   factory MyApi() => _instance;
 ///   MyApi._();
 ///   static final _instance = MyApi._();
-mixin IApi {
+abstract class IApi {
   ///Headers to attach to the request. Usually String or List<String>.
-  final Json headers = <String, dynamic>{};
+  Map<String, dynamic> get headers;
 
   ///Changes the baseUrl.
   set baseUrl(String url);
 
   ///A [get] function that returns [T].
-  Future<T> get<T>(String url);
+  Future<T> get<T>(String path);
 
   ///A [post] function that returns [T].
-  Future<T> post<T>(String url, [data]);
+  Future<T> post<T>(String path, [data]);
+}
+
+/// Used only for internal projects.
+abstract class IApiResponse {
+  ///Headers to attach to the request. Usually String or List<String>.
+  Map<String, dynamic> get headers;
+
+  ///Changes the baseUrl.
+  set baseUrl(String url);
+
+  ///A [get] function that returns [T].
+  Future<ApiResponse<T>> get<T>(String path);
+
+  ///A [post] function that returns [T].
+  Future<ApiResponse<T>> post<T>(String path, [data]);
 }
 
 extension IApiExt on IApi {
@@ -29,7 +44,7 @@ extension IApiExt on IApi {
       : headers['authorization'] = token;
 }
 
-class MockApi with IApi {
+class MockApi implements IApi {
   @override
   Future<T> get<T>(String url) async => 'data' as T;
 
@@ -38,10 +53,70 @@ class MockApi with IApi {
 
   @override
   set baseUrl(String url) {}
+
+  @override
+  // TODO: implement headers
+  Map<String, dynamic> get headers => throw UnimplementedError();
 }
 
 extension MockApiX on MockApi {
   Future<String> getJString() async => '"Hello, World!"';
   Future<String> getJList() async => '["apple", "banana", "orange"]';
   Future<String> getJMap() async => '{"name": "Antony"}';
+}
+
+///Modelo padrão para todas as requisições recebidas via API.
+class ApiResponse<T> {
+  const ApiResponse({
+    required this.result,
+    required this.message,
+    required this.content,
+    required this.token,
+  });
+
+  final bool result;
+  final String message;
+  final List<T> content;
+  final String? token;
+
+  ///Gets the single element data.
+  T? get data => isSingle ? content.first : null;
+
+  ///Checks if one single data.
+  bool get isSingle => content.length == 1;
+
+  // ignore: sort_constructors_first
+  ApiResponse.fromJson(Json json)
+      : result = json['result'],
+        message = json['message'],
+        content = json['content'],
+        token = json['token'];
+
+  Json toJson() {
+    final data = <String, dynamic>{};
+    data['result'] = result;
+    data['message'] = message;
+    data['content'] = content;
+    return data;
+  }
+}
+
+@immutable
+class ApiException implements Exception {
+  const ApiException(this.response, [this.data]);
+  ApiException.message(String message, [this.data])
+      : response = ApiResponse(
+          result: false,
+          message: message,
+          content: [null],
+          token: null,
+        );
+
+  final ApiResponse response;
+  final dynamic data;
+
+  bool get result => response.result;
+  String get message => response.message;
+  List get content => response.content;
+  String? get token => response.token;
 }

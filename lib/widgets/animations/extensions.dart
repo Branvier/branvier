c0d61218514e7236b17841266aa0f72fd76806d0
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-04-14 11:22:34 -0300

    Added AnimationBuilderX with extension and widgets

---
+++ lib/widgets/animations/extensions.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+import 'animations.dart';
+import 'animated_builder.dart';
+
+const _defaultDuration = Duration(seconds: 2);
+const _defaultDelay = Duration.zero;
+
+extension AnimationExtension on Widget {
+  AnimatedBuilderX? get _currentAnimation =>
+      (this is AnimatedBuilderX) ? this as AnimatedBuilderX : null;
+
+  AnimatedBuilderX fadeIn({
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    assert(isSequential || this is! FadeOutAnimation,
+        'Can not use fadeOut + fadeIn when isSequential is false');
+
+    return FadeInAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX fadeOut({
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    assert(isSequential || this is! FadeInAnimation,
+        'Can not use fadeOut() + fadeIn when isSequential is false');
+
+    return FadeOutAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX rotate({
+    required double begin,
+    required double end,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return RotateAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX scale({
+    required double begin,
+    required double end,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return ScaleAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX slide({
+    required OffsetBuilder offset,
+    double begin = 0,
+    double end = 1,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return SlideAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      offsetBuild: offset,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX bounce({
+    required double begin,
+    required double end,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return BounceAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX spin({
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return SpinAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX size({
+    required double begin,
+    required double end,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return SizeAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX blur({
+    double begin = 0,
+    double end = 15,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return BlurAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX flip({
+    double begin = 0,
+    double end = 1,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return FlipAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  AnimatedBuilderX wave({
+    double begin = 0,
+    double end = 1,
+    Duration duration = _defaultDuration,
+    Duration delay = _defaultDelay,
+    ValueSetter<AnimationController>? onComplete,
+    bool isSequential = false,
+  }) {
+    return WaveAnimation(
+      duration: duration,
+      delay: _getDelay(isSequential, delay),
+      begin: begin,
+      end: end,
+      onComplete: onComplete,
+      child: this,
+    );
+  }
+
+  Duration _getDelay(bool isSequential, Duration delay) {
+    assert(!(isSequential && delay != Duration.zero),
+        "Error: When isSequential is true, delay must be non-zero. Context: isSequential: $isSequential delay: $delay");
+
+    return isSequential
+        ? (_currentAnimation?.totalDuration ?? Duration.zero)
+        : delay;
+  }
+}

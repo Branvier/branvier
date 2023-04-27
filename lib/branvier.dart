5451f0451dea9558ffc7e2e6c35027fa05d8d1e0
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-04-27 17:04:26 -0300

    Added Snackbar

--- lib/branvier.dart
+++ lib/branvier.dart
@@ -4,11 +4,14 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:developer' as dev;
 import 'dart:math' as math;
+import 'dart:math';
 import 'dart:ui' as ui;
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
